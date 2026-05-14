2f3214921d23c291a7d34e2760bc3e61b7180050
aditya-rawat-09 <rrawataditya2003@gmail.com>
2026-05-14 20:57:38 +0530

    add Tree

---
+++ Tree/Tree.java
@@ -0,0 +1,13 @@
+public class Tree {
+
+    Node root;
+
+    class Node {
+        int data;
+        Node left, right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+}
